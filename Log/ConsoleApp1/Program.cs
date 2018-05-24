25101132e9be041037736dd001a2b8054db44ee8
Maxwell <chinpanggogogo@gmail.com>
2018-05-24 10:53:17 +0800

    V.1

---
+++ Log/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LogExpert;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String l1 = "INFO   | jvm 1    | main    | 2018/04/17 16:16:01.125 | HOTSWAP AGENT: 16:16:01.069 INFO (org.hotswap.agent.watch.nio.TreeWatcherNIO) - Registering directory target C:\\";
+            String l2 = "INFO   | jvm 1    | main    | 2018/04/17 16:24:05.696 | 	at org.springframework.beans.factory.support.DefaultListableBeanFactory.doResolveDependency(DefaultListableBeanFactory.java:790)";
+            String l3 = "INFO   | jvm 1    | main    | 2018/04/17 16:24:26.957 | DEBUG [WrapperSimpleAppMain] [SoapMessageDispatcher] No EndpointAdapters found, using defaults";
+            HybrisColumnizer test = new HybrisColumnizer();
+            string[] res = test.SplitLine(null, l1);
+            
+        }
+    }
+}
---
+++ Log/Log/HybrisColumnizer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LogExpert;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace LogExpert
+{
+
+    public class HybrisColumnizer : ILogLineColumnizer
+    {
+        private Regex _lineRegex = new Regex("(.*)(\\|\\s*.*)(\\|\\s*.*)(\\|\\s*.*)(\\|\\s*.*)(\\s*\\[.*\\])(\\s*\\[.*\\])(\\s*.*)");
+        //INFO   		| jvm 1    		| main   		| 2018/03/19 11:26:07.706 		| INFO  [WrapperSimpleAppMain] [hybrisserver] Starting up hybris server
+       
+
+        public string GetName()
+        {
+            return "Hybris Columnizer";
+        }
+
+        public string GetDescription()
+        {
+            return "Logfile Format used by SAP Hybris.";
+        }
+
+        public int GetColumnCount()
+        {
+            return 5;
+        }
+
+        public string[] GetColumnNames()
+        {
+            return new string[] { "Log Level", "JVM", "   ", "Date/Time", "Message" };
+        }
+
+        public string[] SplitLine(ILogLineColumnizerCallback callback, string line)
+        {
+            string[] cols = new string[5] { "", "", "", "", "" };
+            if (line.Length > 1024)
+            {
+                // spam
+                line = line.Substring(0, 1024);
+                cols[4] = line;
+                return cols;
+            }
+            String[] res = Regex.Split(line, "\\|");
+            //if (_lineRegex.IsMatch(line))
+            if (true)
+            {
+                /*
+                Match match = _lineRegex.Match(line);
+                GroupCollection groups = match.Groups;
+                if (groups.Count == 9)
+                {
+                    cols[0] = groups[1].Value;
+                    cols[1] = groups[2].Value;
+                    cols[2] = groups[3].Value;
+                    cols[3] = groups[4].Value;
+                    cols[4] = groups[5].Value;
+                    cols[5] = groups[6].Value;
+                    cols[6] = groups[7].Value;
+                    cols[7] = groups[8].Value;
+                }
+                else
+                {
+                    cols[7] = line;
+                }
+                */
+                if (res.Length == 5) {
+                    cols[0] = res[0];
+                    cols[1] = res[1];
+                    cols[2] = res[2];
+                    cols[3] = res[3];
+                    cols[4] = res[4];
+                }
+                else
+                {
+                    cols[4] = line;
+                }
+            }
+            else
+            {
+                cols[4] = line;
+            }
+            return cols;
+        }
+
+        public IColumnizedLogLine SplitLine(ILogLineColumnizerCallback callback, ILogLine line)
+        {
+            ColumnizedLogLine columnizedLogLine = new ColumnizedLogLine();
+            columnizedLogLine.LogLine = line; // Add the reference to the LogLine 
+            Column[] columns = Column.CreateColumns(GetColumnCount(), columnizedLogLine);
+            columnizedLogLine.ColumnValues = columns.Select(a => a as IColumn).ToArray();
+            String[] tmp=SplitLine(callback, line.FullLine);
+
+            for(int i = 0; i < columns.Length; i++)
+            {
+                columns[i].FullValue = tmp[i];
+            }
+            return columnizedLogLine;
+        }
+        public bool IsTimeshiftImplemented()
+        {
+            return false;
+        }
+
+        public void SetTimeOffset(int msecOffset)
+        {
+            throw new NotImplementedException(); //Nothing to do here
+        }
+
+        public int GetTimeOffset()
+        {
+            throw new NotImplementedException(); //Nothing to do here
+        }
+
+        public DateTime GetTimestamp(ILogLineColumnizerCallback callback, string line)
+        {
+            throw new NotImplementedException(); //Nothing to do here
+        }
+
+        public void PushValue(ILogLineColumnizerCallback callback, int column, string value, string oldValue)
+        {
+            throw new NotImplementedException(); //Nothing to do here
+        }
+        public DateTime GetTimestamp(ILogLineColumnizerCallback callback, ILogLine line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
